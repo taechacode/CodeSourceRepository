f79a390d6dbe89b7b2040c42e0b459310ed755f4
taechacode <kangtaechan2015@gmail.com>
2022-05-25 23:06:59 +0900

    9655_돌 게임_Java Source Code Upload

---
+++ Baekjoon Online Judge/9655_돌 게임.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n % 2 == 0) {
+			System.out.println("CY");
+		}
+		else {
+			System.out.println("SK");
+		}
+	}
+}
