c6cc621e175d0fe96757f24e634a1130a72de095
taechacode <kangtaechan2015@gmail.com>
2022-06-13 23:46:36 +0900

    5337_웰컴_Java Source Code Upload

---
+++ Baekjoon Online Judge/5337_웰컴.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+	}
+}
