ca66ffaed1603e19e31cbb8ff0a1a607df4acc98
taechacode <kangtaechan2015@gmail.com>
2024-02-07 22:45:22 +0900

    LEVEL2_구명보트_Java Source Code Upload

---
+++ Programmers/LEVEL2_구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length - 1;
+                
+        while(start <= end) {
+            if(people[start] + people[end] <= limit) {
+                start++;
+                end--;
+            } else {
+                end--;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
