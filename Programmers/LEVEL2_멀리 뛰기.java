bce61d47b37efa53658fdba476f41ca73fef227b
taechacode <kangtaechan2015@gmail.com>
2024-02-12 22:28:17 +0900

    LEVEL2_멀리 뛰기_Java Source Code Upload

---
+++ Programmers/LEVEL2_멀리 뛰기.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    static int[] dp;
+    
+    public long solution(int n) {
+        
+        dp = new int[2001];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= 2000; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+    
+}
