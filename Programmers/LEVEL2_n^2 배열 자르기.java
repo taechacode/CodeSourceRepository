9029943a3d8d23c3066fd0303923545a36938cd3
taechacode <kangtaechan2015@gmail.com>
2024-03-21 23:17:19 +0900

    LEVEL2_n^2 배열 자르기_Java Source Code Upload

---
+++ Programmers/LEVEL2_n^2 배열 자르기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        
+        int[] answer = new int[(int)right - (int)left + 1];
+        
+        for(long i = left; i <= right; i++) {
+            long row = i / n;
+            long col = i % n;
+            answer[(int)i - (int)left] = Math.max((int)row, (int)col) + 1;
+
+        }
+        
+        return answer;
+    }
+}
--- README.md
+++ README.md
@@ -274,6 +274,7 @@
 2024년 2월 14일 : LEVEL2 귤 고르기 (Java)    
 2024년 2월 17일 : LEVEL2 연속 부분 수열 합의 개수 (Java), LEVEL2 괄호 회전하기 (Java)    
 2024년 3월 20일 : LEVEL2 할인 행사 (Java)    
+2024년 3월 21일 : LEVEL2 n^2 배열 자르기 (Java)    
 <br/>
 
 ## :orange_book:SW Expert Academy   
